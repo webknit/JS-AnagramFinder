fb7e7261382ae1133f18321cb5f12905fb28a5c5
webknit <contact@webknit.co.uk>
2014-08-11 16:10:13 +0100

    adding initial functionality

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ var Base = Base || {};
 
 Base.Anagram = function()
 {	
-	var words = ["apers", "refgh", "apres", "apres", "asper", "gfhsj", "pares", "sgtay", "parse", "pears", "hsdyc", "prase", "hauis", "rapes", "sgayh", "spear", "qiuys"]; 
+	var words = ["apers", "refgh", "apres", "asper", "gfhsj", "pares", "sgtay", "parse", "pears", "hsdyc", "prase", "hauis", "rapes", "sgayh", "spear", "qiuys"]; 
 
-	var youWord = "spare";
+	var yourWord = "spare";
 
 	var wordsList = $('.words-list');
+	var foundWordsList = $('.found-words-list');
 
 	function init()
 	{
@@ -27,11 +28,38 @@ Base.Anagram = function()
 	{
 		console.log(words);
 
-		for(i=0; i < words.length; i++) {
+		// Sort your word into alphabetical order, removing spaces
+		yourWord = yourWord.split('').sort().join(''); 
 
-			wordsList.append('<li>' + words[i] + '</li>');
+		console.log('your word is ' + yourWord);
 
-		}
+		// Sort the array of words into alphabetical order
+		var alphabeticalOrderWords = words.map(function(word){
+
+			// Sort your word into alphabetical order, removing spaces
+			// Because this is using MAP it adds it to an array
+		  	return word.split('').sort().join('');
+
+		});
+
+		console.log(alphabeticalOrderWords);
+
+		console.log(alphabeticalOrderWords[1]);
+
+		for(i=0; i < alphabeticalOrderWords.length; i++) {
+
+			var loopWord = alphabeticalOrderWords[i];
+
+		    wordsList.append('<li>' + alphabeticalOrderWords[i] + '</li>');
+
+		    if (yourWord == loopWord) {
+
+		    	foundWordsList.append('<li>' + words[i] + '</li>');
+
+		    }
+
+		};
+
 
 	}
 
